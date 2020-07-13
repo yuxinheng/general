9b863f1639a828018d984a868c5c738a9857b353
zhengxm <zhengxm283@126.com>
2020-07-13 18:01:50 +0800

    test modify

--- src/helloWorld/hello.java
+++ src/helloWorld/hello.java
@@ -1,7 +1,17 @@
 package helloWorld;
 
+import java.util.Scanner;
+
 public class hello {
 	public static void main (String[] args) {
 		System.out.println("hello world");
+		System.out.println("Please input your name (abbreviation):");
+		Scanner input=new Scanner(System.in);
+		String str = input.next();
+		if (str.equals("zhengxm")|str.equals("zxm")) {
+			System.out.println("Welcome back master.");
+		}else {
+			System.out.println("You are not the right one, please leave");
+		}
 	}
 }
